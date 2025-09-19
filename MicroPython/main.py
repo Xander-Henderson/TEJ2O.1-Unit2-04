075540b92512aa3d4d974831b85c4e057b76be70
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-09-19 17:30:49 +0000

    shows tempaerture

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,5 +6,13 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
+# our variable for the temperature
+temperature_of_microbit = 0
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        temperature_of_microbit = input.temperature()
+        display.show(str(temperature_of_microbit))
